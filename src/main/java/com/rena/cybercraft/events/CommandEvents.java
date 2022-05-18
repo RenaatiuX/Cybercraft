ed2f386447c02aa6ce2f7f35d7ffa6ec7d3f9f36
RenaatiuX <77760642+RenaatiuX@users.noreply.github.com>
2022-05-18 14:41:06 -0500

    untested command

---
+++ src/main/java/com/rena/cybercraft/events/CommandEvents.java
@@ -0,0 +1,20 @@
+package com.rena.cybercraft.events;
+
+import com.rena.cybercraft.Cybercraft;
+import com.rena.cybercraft.common.command.CommandClearCybercraft;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.server.command.ConfigCommand;
+
+@Mod.EventBusSubscriber(modid = Cybercraft.MOD_ID)
+public class CommandEvents {
+
+    @SubscribeEvent
+    public static void onCommandsRegister(RegisterCommandsEvent event){
+        new CommandClearCybercraft(event.getDispatcher());
+
+        ConfigCommand.register(event.getDispatcher());
+    }
+
+}
